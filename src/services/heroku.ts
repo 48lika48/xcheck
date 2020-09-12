02ddd6298cf729ce61e1aab477702753e172509e
Alexei Kravchuk <alexei.kravchuk21@gmail.com>
2020-09-12 23:37:33 +0300

    feat: all data interfaces and some heroku requests

--- src/services/heroku.ts
+++ src/services/heroku.ts
@@ -1,6 +1,92 @@
 import { HEROKU_URL } from '../constants';
-import { Endpoint, ITask, DataTypes, IUser, ICheckSession, UserRole } from 'src/models';
+import {
+  Endpoint,
+  ITask,
+  DataTypes,
+  IUser,
+  ICheckSession,
+  UserRole,
+  IReviewRequest,
+  IReview,
+} from 'src/models';
 import { getGithubLogin } from './github-auth';
+
+export const getUsers = async () => {
+  const res = await getData(Endpoint.users);
+  return res;
+};
+
+export const addUser = async (user: IUser) => {
+  const res = await addData(Endpoint.users, user);
+  return res;
+};
+
+export const setUserRoles = (userId: string, roles: UserRole[]) => {
+  fetch(`${HEROKU_URL}users/${userId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({ roles }),
+  });
+};
+
+export const getTasks = async () => {
+  const res = await getData(Endpoint.tasks);
+  return res;
+};
+
+export const addTask = async (task: ITask) => {
+  const res = await addData(Endpoint.tasks, task);
+  return res;
+};
+
+export const getCheckSessions = async () => {
+  const res = await getData(Endpoint.checkSessions);
+  return res;
+};
+
+export const addCheckSession = async (checkSession: ICheckSession) => {
+  const res = await addData(Endpoint.checkSessions, checkSession);
+  return res;
+};
+
+export const getReviewRequests = async () => {
+  const res = await getData(Endpoint.reviewRequests);
+  return res;
+};
+
+export const addReviewRequest = async (reviewRequest: IReviewRequest) => {
+  const res = await addData(Endpoint.reviewRequests, reviewRequest);
+  return res;
+};
+
+export const getReviews = async () => {
+  const res = await getData(Endpoint.reviews);
+  return res;
+};
+
+export const addReview = async (review: IReview) => {
+  const res = await addData(Endpoint.reviews, review);
+  return res;
+};
+
+export const getData = async (endpoint: Endpoint) => {
+  const res = await fetch(HEROKU_URL + endpoint);
+  return res;
+};
+
+export const addData = async (endpoint: Endpoint, data: DataTypes) => {
+  const resolve = await fetch(HEROKU_URL + endpoint, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return resolve;
+};
 
 export const checkUser = async () => {
   const usersRes = await getUsers();
@@ -33,72 +119,4 @@ const registerUser = async (githubLogin: string, users: IUser[]) => {
     roles: [localStorage.role || 'student'],
   };
   addUser(user);
-};
-
-export const getUsers = async () => {
-  const res = await getData(Endpoint.users);
-  return res;
-};
-
-export const addUser = async (user: IUser) => {
-  const res = await addData(Endpoint.users, user);
-  return res;
-};
-
-export const setUserRoles = (userId: string, roles: UserRole[]) => {
-  const body = { roles };
-  fetch(`${HEROKU_URL}users/${userId}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: JSON.stringify(body),
-  });
-  console.log('setUserRoles', body);
-};
-
-export const getTasks = async () => {
-  const res = await getData(Endpoint.tasks);
-  return res;
-};
-
-export const addTask = async (task: ITask) => {
-  const res = await addData(Endpoint.tasks, task);
-  return res;
-};
-
-export const getCheckSessions = async () => {
-  const res = await getData(Endpoint.checkSessions);
-  return res;
-};
-
-export const addCheckSession = async (checkSession: ICheckSession) => {
-  const res = await addData(Endpoint.checkSessions, checkSession);
-  return res;
-};
-
-export const getReviewRequests = async () => {
-  const res = await getData(Endpoint.reviewRequests);
-  return res;
-};
-export const getReviews = async () => {
-  const res = await getData(Endpoint.reviews);
-  return res;
-};
-
-export const getData = async (endpoint: Endpoint) => {
-  const res = await fetch(HEROKU_URL + endpoint);
-  return res;
-};
-
-export const addData = async (endpoint: Endpoint, data: DataTypes) => {
-  const resolve = await fetch(HEROKU_URL + endpoint, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-  return resolve;
 };
