cbcc1a9834a006180c9452119945f6a50a74c984
Dzianis Bogdan <thespirit3000@gmail.com>
2020-09-14 20:39:13 +0300

    refactor: store reconfiguration

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,9 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit'
+import reviewsPageReducer from './reducers/reviewsPageSlice'
 
 const rootReducer = combineReducers({
-
+  reviewsPage: reviewsPageReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
-
 export default rootReducer
