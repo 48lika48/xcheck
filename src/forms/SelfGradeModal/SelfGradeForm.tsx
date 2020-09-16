d0f949f6b5fd368817058d86cb6abc9c6806ac43
Vladislav Shevel <vladislav.shevel@gmail.com>
2020-09-16 19:57:11 +0300

    feat: add local state variables and refactor form

--- src/forms/SelfGradeModal/SelfGradeForm.tsx
+++ src/forms/SelfGradeModal/SelfGradeForm.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { loadTasks } from '../../store/actions/selfGradeAction';
-import { SmileTwoTone } from '@ant-design/icons';
+import { SmileTwoTone, CheckCircleTwoTone } from '@ant-design/icons';
 import {
   Spin,
   Space,
   Form,
   Button,
   InputNumber,
+  Input,
   Popover,
   Typography,
   Divider
@@ -15,12 +16,40 @@ import {
 import { Iprops } from './SelfGradeModal';
 
 const { Paragraph, Text } = Typography;
+const { TextArea } = Input;
+
+const inputStyle = {
+  display: "block",
+  maxWidth: "300px",
+  marginTop: "10px",
+}
+
+// type ItemsProps = {
+//   id: string | null
+//   score: number | null
+//   comment: string | null
+// }
+
+// type taskCoreType = {
+//   task: string | null,
+//   items: Array<ItemsProps>
+// }
 
 export const SelfGradeForm: React.FC<Iprops> = (props: any) => {
 
   const dispatch = useDispatch();
 
+  const [comment, setComment] = useState('');
+  const [score, setScore] = useState(0);
+
   useEffect(() => { dispatch(loadTasks(props.taskId)) }, []);
+
+  const onChangeComment = (event: React.FormEvent<HTMLTextAreaElement>): void => {
+    setComment(event.currentTarget.value);
+  }
+  const onChangeScore = (score: any): void => {
+    setScore(score);
+  }
 
   return (
     props.selfGrade.loading
@@ -31,41 +60,53 @@ export const SelfGradeForm: React.FC<Iprops> = (props: any) => {
       </Space>
       :
       <React.Fragment>
-        <Form
-          initialValues={{ remember: false }}
-          onFinish={props.handleConfirmClick}
+        {
+          props.selfGrade.task.items ?
+            props.selfGrade.task.items.map((item: any, i: number) => {
+              return <Form
+                key={i}
+                initialValues={{ remember: false }}
+                onFinish={() => {
+                  alert(`${score} | ${comment} ||| ${props.selfGrade.task.items[0].id}`);
+                }}
+              >
+                <Paragraph><Text strong>{item.category}</Text></Paragraph>
+                <Paragraph>{item.title}</Paragraph>
+                <Form.Item label="Score: ">
+                  <Popover
+                    content={`Min: ${item.minScore} Max: ${item.maxScore}`}
+                    title='Points'
+                    trigger="focus">
+                    <InputNumber
+                      min={item.minScore}
+                      max={item.maxScore}
+                      required={true}
+                      onChange={onChangeScore}
+                    />
+                  </Popover>
+                  <TextArea
+                    placeholder="Comment"
+                    style={inputStyle}
+                    autoSize
+                    onChange={onChangeComment}
+                  />
+                </Form.Item>
+                <Button
+                  htmlType="submit"
+                  icon={<CheckCircleTwoTone twoToneColor="#52c41a" />}
+                >Save comment
+                </Button>
+                <Divider />
+              </Form>
+            })
+            : null
+        }
+        <Button
+          onClick={props.handleConfirmClick}
         >
-          {
-            props.selfGrade.task.items ?
-              props.selfGrade.task.items.map((item: any, i: number) => {
-                return <div key={i}>
-                  <Paragraph><Text strong>{item.category}</Text></Paragraph>
-                  <Paragraph>{item.title}</Paragraph>
-                  <Form.Item label="Score: ">
-                    <Popover
-                      content={`Min: ${item.minScore} Max: ${item.maxScore}`}
-                      title='Points'
-                      trigger="focus">
-                      <InputNumber
-                        defaultValue={0}
-                        min={item.minScore}
-                        max={item.maxScore}
-                        required={true}
-                      />
-                    </Popover>
-                  </Form.Item>
-                  <Divider />
-                </div>
-              })
-              : null
-          }
-          <Form.Item>
-            <Button htmlType="submit">
-              Confirm
+          Confirm
               <SmileTwoTone />
-            </Button>
-          </Form.Item>
-        </Form>
+        </Button>
       </React.Fragment>
   )
 }
