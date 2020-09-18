75dd86b7ce8b1420590e1a23ac8dec2173539249
Vladislav Shevel <vladislav.shevel@gmail.com>
2020-09-18 18:15:46 +0300

    feat:refactor form inputs and validation

--- src/forms/SelfGradeModal/SelfGradeModal.tsx
+++ src/forms/SelfGradeModal/SelfGradeModal.tsx
@@ -3,55 +3,53 @@ import { Button, Modal, message } from 'antd';
 import { SelfGradeForm } from './SelfGradeForm';
 
 export type Iprops = {
-  taskId?: string
-  handleConfirmClick: () => void
-}
+	taskId?: string;
+	handleConfirmClick: () => void;
+};
 
 export const SelfGradeModal: React.FC<{ taskId: string }> = (props: any) => {
+	const [ showModal, setShowModal ] = useState(false);
+	const [ isDisabledButton, setIsDisabledButton ] = useState(true);
 
-  const [showModal, setShowModal] = useState(false);
-  const [isDisabledButton, setIsDisabledButton] = useState(true);
+	const cancelChanges = (): void => {
+		setShowModal(false);
+		setIsDisabledButton(true);
+	};
 
-  const cancelChanges = (): void => {
-    setShowModal(false);
-    setIsDisabledButton(true);
-  }
+	const saveChanges = (): void => {
+		setShowModal(false);
+		message.success('Self-Check saved!');
+	};
 
-  const saveChanges = (): void => {
-    setShowModal(false);
-    message.success('Self-Check saved!');
-  }
+	const handleConfirmClick = (): void => {
+		setIsDisabledButton(false);
+		message.success('Confirmed!');
+	};
 
-  const handleConfirmClick = (): void => {
-    setIsDisabledButton(false);
-    message.success('Confirmed!');
-  }
-
-  return (
-    <div>
-      <Button
-        type="primary"
-        onClick={() => {
-          setShowModal(true);
-        }}
-      >
-        Do Self-Check
-      </Button>
-      <Modal
-        title="Self-Check"
-        centered
-        visible={showModal}
-        okText='Save result'
-        onOk={() => { saveChanges() }}
-        onCancel={() => cancelChanges()}
-        okButtonProps={{ disabled: isDisabledButton }}
-        width={900}
-      >
-        <SelfGradeForm
-          taskId={props.taskId}
-          handleConfirmClick={handleConfirmClick}
-        />
-      </Modal>
-    </div >
-  )
-}
+	return (
+		<div>
+			<Button
+				type="primary"
+				onClick={() => {
+					setShowModal(true);
+				}}
+			>
+				Do Self-Check
+			</Button>
+			<Modal
+				title="Self-Check"
+				centered
+				visible={showModal}
+				okText="Save result"
+				onOk={() => {
+					saveChanges();
+				}}
+				onCancel={() => cancelChanges()}
+				okButtonProps={{ disabled: isDisabledButton }}
+				width={900}
+			>
+				<SelfGradeForm taskId={props.taskId} handleConfirmClick={handleConfirmClick} />
+			</Modal>
+		</div>
+	);
+};
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Layout, Tabs, PageHeader, Button, Tooltip } from 'antd'
+import { Layout, Tabs, PageHeader, Button, Tooltip } from 'antd';
 import { UnorderedListOutlined, PullRequestOutlined, ScheduleTwoTone, LogoutOutlined } from '@ant-design/icons';
 
 import { getGithubUserName, deleteCookie } from '../../services/github-auth';
@@ -12,39 +12,69 @@ const { Footer, Content } = Layout;
 const { TabPane } = Tabs;
 
 export const Main: React.FC<{ logoutHandler: any }> = (props) => {
+	const role = localStorage.getItem('role') || 'student';
+	// const taskId = 'xcheck';
+	const taskId = 'simple-task-v1';
 
-  const role = localStorage.getItem('role') || 'student';
-  const taskId = 'xcheck';
-
-  return (
-    <Layout>
-      <PageHeader
-        className="site-page-header"
-        title="RS School xCheck"
-        subTitle={`${getGithubUserName()} (${role})`}
-        extra={
-          <Tooltip title="logout">
-            <Button type="link" shape="circle"
-              icon={<LogoutOutlined onClick={() => { props.logoutHandler(); deleteCookie() }} />} />
-          </Tooltip>
-        }
-      />
-      <Content>
-        <Tabs defaultActiveKey="1">
-          <TabPane tab={<span><UnorderedListOutlined />Tasks</span>} key="1">
-            Страница тасков
-        </TabPane>
-          <TabPane tab={<span><PullRequestOutlined />Review requests</span>} key="2">
-            <SelfGradeModal taskId={taskId}/>
-            Страница зопроса на ревью
-        </TabPane>
-          <TabPane tab={<span><ScheduleTwoTone />Reviews</span>} key="3">
-            Страница ревью
-        </TabPane>
-        </Tabs>
-      </Content>
-      <Footer>Footer</Footer>
-    </Layout>
-
-  )
-}
+	return (
+		<Layout>
+			<PageHeader
+				className="site-page-header"
+				title="RS School xCheck"
+				subTitle={`${getGithubUserName()} (${role})`}
+				extra={
+					<Tooltip title="logout">
+						<Button
+							type="link"
+							shape="circle"
+							icon={
+								<LogoutOutlined
+									onClick={() => {
+										props.logoutHandler();
+										deleteCookie();
+									}}
+								/>
+							}
+						/>
+					</Tooltip>
+				}
+			/>
+			<Content>
+				<Tabs defaultActiveKey="1">
+					<TabPane
+						tab={
+							<span>
+								<UnorderedListOutlined />Tasks
+							</span>
+						}
+						key="1"
+					>
+						Страница тасков
+					</TabPane>
+					<TabPane
+						tab={
+							<span>
+								<PullRequestOutlined />Review requests
+							</span>
+						}
+						key="2"
+					>
+						<SelfGradeModal taskId={taskId} />
+						Страница зопроса на ревью
+					</TabPane>
+					<TabPane
+						tab={
+							<span>
+								<ScheduleTwoTone />Reviews
+							</span>
+						}
+						key="3"
+					>
+						Страница ревью
+					</TabPane>
+				</Tabs>
+			</Content>
+			<Footer>Footer</Footer>
+		</Layout>
+	);
+};
