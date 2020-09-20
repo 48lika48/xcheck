47bfc75edc720e544007c10d8a6eb73263cd0b7a
Dzianis Bohdan <thespirit3000@gmail.com>
2020-09-20 13:34:44 +0300

    feat: connect to store

--- src/pages/crossSessionsPage/CrossSessionsPage.tsx
+++ src/pages/crossSessionsPage/CrossSessionsPage.tsx
@@ -1,6 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Button, Spin } from 'antd';
+import { RootState } from '../../store/rootReducer';
+import { fetchSessions } from '../../store/reducers/crossSessionsSlice';
+import { PlusOutlined } from '@ant-design/icons';
 
 export const CrossSessionsPage: React.FC = () => {
-  return <></>;
+  const dispatch = useDispatch();
+  const { sessions, loading } = useSelector((state: RootState) => state.crossSessions);
+  const getData = useCallback(() => {
+    dispatch(fetchSessions());
+  }, [dispatch]);
+  useEffect(() => {
+    getData();
+  }, [dispatch, getData]);
+
+  return (
+    <Spin spinning={loading}>
+      <Button
+        onClick={() => console.log('add')}
+        type="primary"
+        style={{
+          marginBottom: 16,
+        }}
+        icon={<PlusOutlined />}
+      >
+        Add a session
+      </Button>
+    </Spin>
+  );
 };
