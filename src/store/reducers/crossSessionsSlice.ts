0f226f1d2501ba6e10ec39c85b9bac11cd32d0be
Dzianis Bohdan <thespirit3000@gmail.com>
2020-09-20 13:35:10 +0300

    feat: loading reducer

--- src/store/reducers/crossSessionsSlice.ts
+++ src/store/reducers/crossSessionsSlice.ts
@@ -1,15 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { ICheckSession } from '../../models';
+import { AppThunk } from '../store';
+import { getCheckSessions } from '../../services/heroku';
 
 interface ICrossSessionsSlice {
+  loading: boolean;
   error: string | null;
+  sessions: ICheckSession[];
 }
 const initialState: ICrossSessionsSlice = {
+  loading: false,
   error: null,
+  sessions: [],
 };
 const crossSessionSlice = createSlice({
   name: 'crossSessions',
   initialState,
-  reducers: {},
+  reducers: {
+    startLoading(state) {
+      state.loading = true;
+    },
+    endLoading(state) {
+      state.loading = false;
+    },
+  },
 });
+
+export const { startLoading, endLoading } = crossSessionSlice.actions;
+
+export const fetchSessions = (): AppThunk => async (dispatch) => {
+  try {
+    dispatch(startLoading());
+    const sessions = await getCheckSessions();
+    console.log(sessions);
+    dispatch(endLoading());
+  } catch (err) {}
+};
 
 export default crossSessionSlice.reducer;
