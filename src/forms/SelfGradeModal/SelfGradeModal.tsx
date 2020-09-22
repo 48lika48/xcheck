6d6e693d5dd04cc1abb28a2ac70a0867fe969b61
Maksim Diubo <diubomaksim@gmail.com>
2020-09-22 17:26:51 +0300

    fix: replace tabs whith spaces

--- src/forms/SelfGradeModal/SelfGradeModal.tsx
+++ src/forms/SelfGradeModal/SelfGradeModal.tsx
@@ -7,54 +7,54 @@ import { setSelfGrade } from 'src/store/reducers/reviewRequestSlice';
 const WARNING_MESSAGE = 'Check carefully! Enter and save all items.';
 
 type SelfGradeModalProps = {
-	taskId: string | null;
-	isSelfGradeShow: boolean;
-	selfGradeHandler: () => void;
+  taskId: string | null;
+  isSelfGradeShow: boolean;
+  selfGradeHandler: () => void;
 }
 
 export const SelfGradeModal: React.FC<SelfGradeModalProps> = ({ taskId, selfGradeHandler, isSelfGradeShow }) => {
-	const [showModal, setShowModal] = useState(isSelfGradeShow);
-	const [isDisabledButton, setIsDisabledButton] = useState(true);
+  const [showModal, setShowModal] = useState(isSelfGradeShow);
+  const [isDisabledButton, setIsDisabledButton] = useState(true);
 
-	const dispatch = useDispatch();
-	const { task } = useSelector((state: RootState) => state.selfGradeSlice);
-	const { taskScore } = useSelector((state: RootState) => state.selfGradeSlice);
+  const dispatch = useDispatch();
+  const { task } = useSelector((state: RootState) => state.selfGradeSlice);
+  const { taskScore } = useSelector((state: RootState) => state.selfGradeSlice);
 
-	const cancelChanges = (): void => {
-		setShowModal(false);
-		setIsDisabledButton(true);
-		selfGradeHandler();
-	};
+  const cancelChanges = (): void => {
+    setShowModal(false);
+    setIsDisabledButton(true);
+    selfGradeHandler();
+  };
 
-	const saveChanges = (): void => {
-		setShowModal(false);
-		selfGradeHandler();
-	};
+  const saveChanges = (): void => {
+    setShowModal(false);
+    selfGradeHandler();
+  };
 
-	const handleEndCheck = (): void => {
-		if (task?.items && taskScore.items.length !== task?.items.length) {
-			message.warning(WARNING_MESSAGE);
-			return;
-		}
-		dispatch(setSelfGrade(taskScore));
-		setIsDisabledButton(false);
-		message.success('Check Saved!');
-	};
+  const handleEndCheck = (): void => {
+    if (task?.items && taskScore.items.length !== task?.items.length) {
+      message.warning(WARNING_MESSAGE);
+      return;
+    }
+    dispatch(setSelfGrade(taskScore));
+    setIsDisabledButton(false);
+    message.success('Check Saved!');
+  };
 
-	return (
-		<div>
-			<Modal
-				title="Check task"
-				centered
-				visible={showModal}
-				okText="OK"
-				onOk={saveChanges}
-				onCancel={cancelChanges}
-				okButtonProps={{ disabled: isDisabledButton }}
-				width={900}
-			>
-				<SelfGradeForm taskId={taskId} handleEndCheck={handleEndCheck} />
-			</Modal>
-		</div>
-	);
+  return (
+    <div>
+      <Modal
+        title="Check task"
+        centered
+        visible={showModal}
+        okText="OK"
+        onOk={saveChanges}
+        onCancel={cancelChanges}
+        okButtonProps={{ disabled: isDisabledButton }}
+        width={900}
+      >
+        <SelfGradeForm taskId={taskId} handleEndCheck={handleEndCheck} />
+      </Modal>
+    </div>
+  );
 };
