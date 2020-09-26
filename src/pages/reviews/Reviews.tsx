c392e4df78f8a6c2b3d337df9c343fa403876292
Dzianis Bohdan <thespirit3000@gmail.com>
2020-09-27 00:04:11 +0300

    feat: createForm handler

--- src/pages/reviews/Reviews.tsx
+++ src/pages/reviews/Reviews.tsx
@@ -9,15 +9,25 @@ import {
   fetchRequestsToReview,
   fetchReviewsByAuthor,
   fetchReviewsRequests,
+  resetRequests,
 } from '../../store/reducers/reviewsPageSlice';
 import { ReloadOutlined } from '@ant-design/icons';
 import { RequestSelector } from './components/requestSelector';
+import { ReviewCreatePage } from './components/reviewCreatePage/ReviewCreatePage';
+import { hideModal, showModal, setRequest } from 'src/store/reducers/reviewCreateSlice';
+import { IReviewRequest } from '../../models';
+
+function setRequestForForm(requestArray: IReviewRequest[], id: string) {
+  return requestArray.find((item) => item.id === id);
+}
 
 export const ReviewPage: React.FC = () => {
   const dispatch = useDispatch();
   const { taskLoading, reviews, requests, error, requestsForReview } = useSelector(
     (state: RootState) => state.reviewsPage
   );
+  const { allTasks } = useSelector((state: RootState) => state.tasks);
+  const { isShowModal, request } = useSelector((state: RootState) => state.reviewCreate);
   const getData = useCallback(() => {
     dispatch(fetchReviewsByAuthor());
     dispatch(fetchReviewsRequests());
@@ -26,6 +36,22 @@ export const ReviewPage: React.FC = () => {
   useEffect(() => {
     getData();
   }, [dispatch, getData]);
+  function closeHandler() {
+    dispatch(hideModal());
+    dispatch(setRequest(null));
+    dispatch(resetRequests());
+    // if (isEdit) {
+    //   dispatch(endEdit());
+    // }
+    getData();
+  }
+
+  function changeHandler(value: string) {
+    console.log(value);
+    dispatch(setRequest(setRequestForForm(requestsForReview, value)));
+    dispatch(showModal());
+  }
+
   const columns = [
     {
       title: 'Reviewed Student',
@@ -103,13 +129,24 @@ export const ReviewPage: React.FC = () => {
   return (
     <Spin spinning={taskLoading}>
       <Space size="middle">
-        <Badge count={requests.length}>
+        <Badge count={requestsForReview.length}>
           <Tooltip placement="topLeft" title={`You can create ${requests.length} more reviews`}>
-            <RequestSelector requests={requestsForReview} />
+            <RequestSelector
+              requests={requestsForReview}
+              select={changeHandler}
+              loading={taskLoading}
+            />
           </Tooltip>
         </Badge>
       </Space>
       {error && <h1>{`Error: ${error}`}</h1>}
+      <ReviewCreatePage
+        isEdit={false}
+        isShowModal={isShowModal}
+        closeManager={closeHandler}
+        reviewRequest={request}
+        task={allTasks}
+      />
       <Table columns={columns} dataSource={reviews} rowKey="id" />
       <Button onClick={() => getData()} icon={<ReloadOutlined />}>
         Reload
